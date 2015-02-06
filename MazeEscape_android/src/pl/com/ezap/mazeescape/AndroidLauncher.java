ba5fd67f22d5189d369a246a5c76cfcadcd8f000
Zbigniew Piecuch <zpiecuch81@gmail.com>
2015-02-06 23:40:35 +0100

    Added add mob advertisements.

--- MazeEscape_android/src/pl/com/ezap/mazeescape/AndroidLauncher.java
+++ MazeEscape_android/src/pl/com/ezap/mazeescape/AndroidLauncher.java
@@ -2,9 +2,16 @@ package pl.com.ezap.mazeescape;
 
 import pl.com.ezap.mazeescape.main.Game;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -13,7 +20,28 @@ public class AndroidLauncher extends AndroidApplication {
 
 		RateApp.app_launched( this );
 
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Game(), config);
+		View gameView = initializeForView(new Game(), config);
+		AdView adView = new AdView(this); // Put in your secret key here
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId("ca-app-pub-4163298076894798/2080813469");
+		AdRequest.Builder adBuilder = new AdRequest.Builder();
+		adBuilder.addTestDevice("515D01A11F47A111FC078E270323739B");
+		adView.loadAd(adBuilder.build());
+
+		RelativeLayout.LayoutParams adParams = 
+				new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, 
+						RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		RelativeLayout layout = new RelativeLayout(this);
+		layout.addView(gameView);
+		layout.addView(adView, adParams);
+		setContentView(layout);
 	}
 }
